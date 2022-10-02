5535e50c4fc4d822c70c38a28152aee54f534c67
Егор <lerik1ept@gmail.com>
2022-10-02 17:37:28 +0300

    Добавил UserControl и часть функционала GUI

---
+++ ASMaIoP/UserControl/WaitLogins.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ASMaIoP;
+
+namespace ASMaIoP.UserControl
+{
+    /// <summary>
+    /// Логика взаимодействия для WaitLogins.xaml
+    /// </summary>
+    public partial class WaitLogins : System.Windows.Controls.UserControl
+    {
+        MainWindow wnd;
+
+        public WaitLogins(MainWindow wnd)
+        {
+            InitializeComponent();
+            this.wnd = wnd;
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            wnd.WindowEnabled();
+        }
+    }
+}
