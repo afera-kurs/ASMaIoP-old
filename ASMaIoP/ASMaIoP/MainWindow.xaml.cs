cda708e4161bf1a0678d4d97d720276a91b214d1
caratovchanin <lerik1ept@gmail.com>
2022-10-06 16:49:10 +0300

    Добавил страницы для каждой кнопки

---
+++ ASMaIoP/ASMaIoP/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ASMaIoP.UserControl;
+
+namespace ASMaIoP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public void AlignmentTopLeft()
+        {
+            ContentView.HorizontalAlignment = HorizontalAlignment.Left;
+            ContentView.VerticalAlignment = VerticalAlignment.Top;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContentView.Content = new WaitLogins(this); //Отправляем в форму WaitLogins ссылку на эту форму
+        }
+
+        public void WindowEnabled() //Метод активируйщий menu после авторизации
+        {
+            MainMenu.IsEnabled = true;
+            ContentView.Content = null;
+        }
+
+        private void Menu_MyProfile_Click(object sender, RoutedEventArgs e)
+        {
+            AlignmentTopLeft();
+            ContentView.Content = new MyProfile();
+        }
+        private void Menu_CreateProfile_Click(object sender, RoutedEventArgs e)
+        {
+            AlignmentTopLeft();
+            ContentView.Content = new CreateProfile();
+        }
+        private void Menu_ListView_Click(object sender, RoutedEventArgs e)
+        {
+            AlignmentTopLeft();
+            ContentView.Content = new TaskList();
+        }
+        private void Menu_CreateList_Click(object sender, RoutedEventArgs e)
+        {
+            AlignmentTopLeft();
+            Content = new CreateTask();
+        }
+        private void Menu_InvetaryOpen_Click(object sender, RoutedEventArgs e)
+        {
+            AlignmentTopLeft();
+            Content = new Inventory();
+        }
+        private void Menu_AddInvetary_CLick(object sender, RoutedEventArgs e)
+        {
+            AlignmentTopLeft();
+            Content = new AddItemsInventory();
+        }
+    }
+}
+
---
+++ ASMaIoP/ASMaIoP/UserControl/MyProfile.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ASMaIoP;
+
+namespace ASMaIoP.UserControl
+{
+    /// <summary>
+    /// Логика взаимодействия для MyProfile.xaml
+    /// </summary>
+    public partial class MyProfile : System.Windows.Controls.UserControl
+    {
+        public MyProfile()
+        {
+            InitializeComponent();
+            ASMaIoP.General.Config cfg = new ASMaIoP.General.Config();
+            WriteChtoto(/* cfg[initials], cfg[ID], cfg[lvl] */);
+        }
+        public void WriteChtoto(/* string initials, int ID, int lvl */)
+        {
+            ListBoxPersonData.Items[1] = "imy"/*initials*/;
+            ListBoxPersonData.Items[2] = "imy"/*ID*/;
+            ListBoxPersonData.Items[3] = "imy"/*lvl*/;
+        }
+        private void UpdatePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
